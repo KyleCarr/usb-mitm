1627f4452aaed4f1ab71685a40d6fe758eacf869
Kyle Carr <kyle.carr98@gmail.com>
2023-03-11 18:22:06 +0000

    Created a symbolic USB connection between the host computer and usb SDA1

--- usb-connection.bash
+++
@@ -1,4 +0,0 @@
-echo device | sudo tee /sys/bus/platform/drivers/dwc3-meson-g12a/d0078080.usb/usb_role/d0078080.usb-role-switch/role
-echo c9100000.usb | sudo tee /sys/bus/platform/drivers/dwc2/unbind
-echo c9100000.usb | sudo tee /sys/bus/platform/drivers/dwc2/bind
-sudo modprobe g_ether host_addr=00:dc:c8:f7:75:05 dev_addr=00:dd:dc:eb:6d:f1
