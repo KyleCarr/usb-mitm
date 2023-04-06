db18e541fd826baffc1b95e9da76b1d38bdd0d67
Kyle Carr <kyle.carr98@gmail.com>
2023-04-06 18:32:35 +0000

    Updated filetransfer.bash to send files over

--- filetransfer.bash
+++ filetransfer.bash
@@ -6,10 +6,10 @@ IP=$(<IP.txt)
 
 mkdir -p /file_holder
 for FILE in /mnt/mydrive/*; do
-    cp $FILE /file_holder
+    cp "$FILE" /file_holder
 done
 
-
+IFS=$'\n'
 for FILE in $(ls -S /file_holder*); do
-    sshpass -p "$password" scp -v $FILE "$username"@"$IP":.
+    sshpass -p "$password" scp /file_holder/"$FILE" "$username"@"$IP":~/files
 done
